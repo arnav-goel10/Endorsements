3111e518df4701eaa8dcd223657bc6b1f24a9eba
Arnav Goel <arnav.goel1@Outlook.com>
2023-12-18 22:35:21 +0530

    Upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // javascript
+const fromEl = document.getElementById('from');
+const toEl = document.getElementById('to');
+const testimonialEl = document.getElementById('testimonial');
+
+function published() {
+    const fromval = fromEl.value
+    const toval = toEl.value
+    const testimonialval = testimonialEl.value
+    const testimonialobj = {
+        from: fromval,
+        to: toval,
+        testimonial: testimonialval
+    }
+    clear()
+}
+
+function clear() {
+    fromEl.value = ''
+    toEl.value = ''
+    testimonialEl.value = ''
+}
+
