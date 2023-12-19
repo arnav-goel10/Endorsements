27ba45b248cc9de963b552494d91fe3bfb77446e
Arnav Goel <arnav.goel1@Outlook.com>
2023-12-19 17:51:16 +0530

    g

--- index.js
+++ index.js
@@ -60,47 +60,82 @@ function cleartestimonials() {
     lsendEl.innerHTML = ''
 }
 
-function addtestimonial(obj,key) {
-    let testin = document.createElement('div')
-    const likeEl = document.getElementsByClassName('likes')
-    const individualEl = document.getElementsByClassName('individual')
-    const  newEl = document.createElement("li")
-    const newTo = document.createElement("div")
-    const newFrom = document.createElement("div")
-    const newTest = document.createElement("div")
-    const newLike = document.createElement("button")
-    newEl.className = "individual"
-    newTo.className = "totest"
-    newFrom.className = "fromtest"
-    newTest.className = "testtest"
-    newLike.className = "likes"
-    newTo.innerHTML = `<p>To ${obj.to}</p>`
-    newTest.innerHTML = `<p>${obj.testimonial}</p>`
-    newLike.textContent = `${obj.likecount==undefined?0:obj.likecount} 🩷`
-    newFrom.innerHTML = `From ${obj.sender}`
-    newFrom.appendChild(newLike)
-    newEl.appendChild(newTo)
-    newEl.appendChild(newTest)
-    newEl.appendChild(newFrom)
-    lsendEl.prepend(newEl)
-    const currentCount = Number(newLike.textContent.split(" ")[0])
-    newLike.addEventListener("click", function() {
-        startlike(currentCount,key,obj)         
-    })  
-    newEl.addEventListener("dblclick", function() {
-        startlike(currentCount,key,obj)
-      });
-    
+function addtestimonial(obj, key) {
+  let testin = document.createElement('div');
+  const likeEl = document.getElementsByClassName('likes');
+  const individualEl = document.getElementsByClassName('individual');
+  const newEl = document.createElement("li");
+  const newTo = document.createElement("div");
+  const newFrom = document.createElement("div");
+  const newTest = document.createElement("div");
+  const newLike = document.createElement("button");
+  newEl.className = "individual";
+  newTo.className = "totest";
+  newFrom.className = "fromtest";
+  newTest.className = "testtest";
+  newLike.className = "likes";
+  newTo.innerHTML = `<p>To ${obj.to}</p>`;
+  newTest.innerHTML = `<p>${obj.testimonial}</p>`;
+  const currentCount = obj.likecount || 0;
+  newLike.textContent = `${currentCount} 🩷`;
+  newFrom.innerHTML = `From ${obj.sender}`;
+  newFrom.appendChild(newLike);
+  newEl.appendChild(newTo);
+  newEl.appendChild(newTest);
+  newEl.appendChild(newFrom);
+  lsendEl.prepend(newEl);
+
+  const isLikedPost = isLiked(key); // Check if post is liked using local storage function
+
+  // Update like button styling based on like state
+  if (isLikedPost) {
+    newLike.classList.add("liked");
+  } else {
+    newLike.classList.remove("liked");
+  }
+
+  newLike.addEventListener("click", function() {
+    startlike(currentCount, key, obj);
+  });
+
+  newEl.addEventListener("dblclick", function() {
+    startlike(currentCount, key, obj);
+  });
 }
 
 function updateLikeCount(key, currentCount,obj) {
     const newObj = {
         ...obj,
-        likecount: currentCount + 1
+        likecount: currentCount
     };
     set(ref(database, `EndorsementList/${key}`), JSON.stringify(newObj));
 }
 
-function startlike(xyz,key1,obj1) {
-    updateLikeCount(key1, xyz,obj1)
+function startlike(xyz, key1, obj1) {
+  const hasLikedBefore = isLiked(key1); // Check if post is liked in local storage
+
+  if (hasLikedBefore) {
+    updateLikeCount(key1, xyz - 1, obj1); // Update like count -1
+    removeFromLikedPosts(key1); // Remove from local storage liked posts
+  } else {
+    updateLikeCount(key1, xyz + 1, obj1); // Update like count +1
+    addToLikedPosts(key1); // Add to local storage liked posts
+  }
 }
+
+function isLiked(key) {
+    const likedPosts = JSON.parse(localStorage.getItem("likedPosts") || "{}");
+    return likedPosts[key] === true;
+  }
+
+  function addToLikedPosts(key) {
+    const likedPosts = JSON.parse(localStorage.getItem("likedPosts") || "{}");
+    likedPosts[key] = true;
+    localStorage.setItem("likedPosts", JSON.stringify(likedPosts));
+  }
+  
+  function removeFromLikedPosts(key) {
+    const likedPosts = JSON.parse(localStorage.getItem("likedPosts") || "{}");
+    delete likedPosts[key];
+    localStorage.setItem("likedPosts", JSON.stringify(likedPosts));
+    }
